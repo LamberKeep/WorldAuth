72767cf76fef05789020850eae91acacb23a0ef2
Lamb's Keeper <LamberKeep@gmail.com>
2021-12-31 14:38:35 +0300

    data manager change & optimization

--- src/main/java/com/myvnc/exo/worldauth/DataManager.java
+++ src/main/java/com/myvnc/exo/worldauth/DataManager.java
@@ -57,23 +57,4 @@ public class DataManager {
             this.plugin.saveResource("data.yml", false);
         }
     }
-
-    public void saveInventory(Player p) throws IOException {
-        File f = new File(plugin.getDataFolder().getAbsolutePath(), p.getName() + ".yml");
-        FileConfiguration c = YamlConfiguration.loadConfiguration(f);
-        c.set("inventory.armor", p.getInventory().getArmorContents());
-        c.set("inventory.content", p.getInventory().getContents());
-        c.save(f);
-    }
-
-    @SuppressWarnings("unchecked")
-    public void restoreInventory(Player p) {
-        File f = new File(plugin.getDataFolder().getAbsolutePath(), p.getName() + ".yml");
-        FileConfiguration c = YamlConfiguration.loadConfiguration(f);
-        ItemStack[] content = ((List<ItemStack>) Objects.requireNonNull(c.get("inventory.armor"))).toArray(new ItemStack[0]);
-        p.getInventory().setArmorContents(content);
-        content = ((List<ItemStack>) Objects.requireNonNull(c.get("inventory.content"))).toArray(new ItemStack[0]);
-        p.getInventory().setContents(content);
-    }
-
 }
--- src/main/java/com/myvnc/exo/worldauth/WorldAuth.java
+++ src/main/java/com/myvnc/exo/worldauth/WorldAuth.java
@@ -8,19 +8,17 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Objects;
 
 public final class WorldAuth extends JavaPlugin implements Listener {
 
-    HashMap<String, Long> Session  = new HashMap<>(); // Sessions
-
+    HashMap<String, Long> session  = new HashMap<>(); // Sessions
     public DataManager data;
-
 
     @Override
     public void onEnable() {
@@ -39,7 +37,7 @@ public final class WorldAuth extends JavaPlugin implements Listener {
         Player p = e.getPlayer();
         String ip = Objects.requireNonNull(p.getAddress()).getAddress().toString();
 
-        if (!Session.containsKey(ip) && System.currentTimeMillis() >= Session.getOrDefault(ip, (long) 0)) {
+        if (!session.containsKey(ip) && System.currentTimeMillis() >= session.getOrDefault(ip, (long) 0)) {
             p.teleport(Objects.requireNonNull(Bukkit.getWorld(Objects.requireNonNull(getConfig().getString("worlds.auth")))).getSpawnLocation());
             p.getInventory().clear();
             p.setGameMode(GameMode.ADVENTURE);
@@ -48,7 +46,7 @@ public final class WorldAuth extends JavaPlugin implements Listener {
     }
 
     @EventHandler
-    public void onLeave(PlayerQuitEvent e) throws IOException {
+    public void onLeave(PlayerQuitEvent e) {
 
         Player p = e.getPlayer();
         String n = p.getName();
@@ -57,42 +55,50 @@ public final class WorldAuth extends JavaPlugin implements Listener {
         e.setQuitMessage(null);
 
         if (p.getWorld() != Bukkit.getWorld(Objects.requireNonNull(getConfig().getString("worlds.auth")))) {
+            session.put(Objects.requireNonNull(p.getAddress()).getAddress().toString(), System.currentTimeMillis() + 3600000);
 
-            Session.put(Objects.requireNonNull(p.getAddress()).getAddress().toString(), System.currentTimeMillis() + 3600000);
-
-            data.getConfig().set("data."+n+".gamemode" , p.getGameMode().toString());
-            data.getConfig().set("data."+n+".location.World" , Objects.requireNonNull(loc.getWorld()).getName());
-            data.getConfig().set("data."+n+".location.X" , loc.getX());
-            data.getConfig().set("data."+n+".location.Y" , loc.getY());
-            data.getConfig().set("data."+n+".location.Z" , loc.getZ());
-            data.getConfig().set("data."+n+".location.Yaw" , loc.getYaw());
-            data.getConfig().set("data."+n+".location.Pitch" , loc.getPitch());
-            data.saveInventory(p);
+            // Save data location and inventory.
+            data.getConfig().set("data."+n+".gamemode", p.getGameMode().toString());
+            data.getConfig().set("data."+n+".location.World", Objects.requireNonNull(loc.getWorld()).getName());
+            data.getConfig().set("data."+n+".location.X", loc.getX());
+            data.getConfig().set("data."+n+".location.Y", loc.getY());
+            data.getConfig().set("data."+n+".location.Z", loc.getZ());
+            data.getConfig().set("data."+n+".location.Yaw", loc.getYaw());
+            data.getConfig().set("data."+n+".location.Pitch", loc.getPitch());
+            data.getConfig().set("data."+n+".inventory.Armor", p.getInventory().getArmorContents());
+            data.getConfig().set("data."+n+".inventory.Content", p.getInventory().getContents());
             data.saveConfig();
-
         }
     }
 
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         Player p = (Player) sender;
-
+        String n = p.getName();
         if (label.equalsIgnoreCase("login") || label.equalsIgnoreCase("l")) {
-            String loc = "data."+sender.getName()+".location";
             if (p.getWorld() == Bukkit.getWorld(Objects.requireNonNull(getConfig().getString("worlds.auth")))) {
                 if (args.length == 0)
                     sender.sendMessage(ChatColor.RED + "Usage: /login <password>");
                 else {
-                    if (this.data.getConfig().getString("data." + p.getName() + ".password") == null)
+                    if (this.data.getConfig().getString("data."+n+".password") == null)
                         sender.sendMessage(ChatColor.RED + "Not registered yet, use /register <password>");
                     else {
-                        if (args[0].equals(this.data.getConfig().getString("data." + p.getName() + ".password"))) {
+                        if (args[0].equals(this.data.getConfig().getString("data."+n+".password"))) {
                             sender.sendMessage(ChatColor.GREEN + "Log in successfully.");
-                            if (this.data.getConfig().getString(loc) != null) {
-                                Objects.requireNonNull(p.getPlayer()).teleport(new Location(Bukkit.getServer().getWorld(Objects.requireNonNull(data.getConfig().getString("data." + Objects.requireNonNull(p.getPlayer()).getName() + ".location.World"))), data.getConfig().getDouble("data." + Objects.requireNonNull(p.getPlayer()).getName() + ".location.X"), data.getConfig().getDouble(".location.Y"), data.getConfig().getDouble("data." + Objects.requireNonNull(p.getPlayer()).getName() + ".location.Z"), (float) data.getConfig().getDouble("data." + Objects.requireNonNull(p.getPlayer()).getName() + ".location.Yaw"), (float) data.getConfig().getDouble("data." + Objects.requireNonNull(p.getPlayer()).getName() + ".location.Pitch")));
-                                data.restoreInventory(p);
-                            } else
-                                Objects.requireNonNull(p.getPlayer()).teleport(Objects.requireNonNull(Bukkit.getWorld(Objects.requireNonNull(getConfig().getString("worlds.hub-or-survival")))).getSpawnLocation()); // first join
-                            Bukkit.broadcastMessage(ChatColor.YELLOW + Objects.requireNonNull(p.getPlayer()).getDisplayName() + ChatColor.YELLOW + " joined to the server");
+
+                            // location restore
+                            Objects.requireNonNull(p.getPlayer()).teleport(new Location(Bukkit.getServer().getWorld(
+                                    Objects.requireNonNull(data.getConfig().getString("data."+n+".location.World"))),
+                                    data.getConfig().getDouble("data."+n+".location.X"),
+                                    data.getConfig().getDouble("data."+n+".location.Y"),
+                                    data.getConfig().getDouble("data."+n+".location.Z"),
+                                    (float) data.getConfig().getDouble("data."+n+".location.Yaw"),
+                                    (float) data.getConfig().getDouble("data."+n+".location.Pitch")));
+
+                            // item restore
+                            p.getInventory().setArmorContents((ItemStack[]) data.getConfig().get("inventory.Armor"));
+                            p.getInventory().setContents((ItemStack[]) Objects.requireNonNull(data.getConfig().get("inventory.Content")));
+
+                            Bukkit.broadcastMessage(ChatColor.YELLOW +p.getDisplayName()+ChatColor.YELLOW+" joined to the server");
                         } else {
                             sender.sendMessage(ChatColor.RED + "Bad password, try again!");
                         }
@@ -111,6 +117,7 @@ public final class WorldAuth extends JavaPlugin implements Listener {
                         data.getConfig().set("data." + sender.getName() + ".password", args[0]);
                         data.saveConfig();
                         sender.sendMessage(ChatColor.GREEN + "Registered successfully.");
+                        Objects.requireNonNull(p.getPlayer()).teleport(Objects.requireNonNull(Bukkit.getWorld(Objects.requireNonNull(getConfig().getString("worlds.hub-or-survival")))).getSpawnLocation());
                     } else
                         sender.sendMessage(ChatColor.RED + "Already registered, use /login <password>");
                 }
@@ -120,7 +127,7 @@ public final class WorldAuth extends JavaPlugin implements Listener {
 
         if (label.equalsIgnoreCase("logout")) {
             p.kickPlayer(Color.YELLOW+"Auth"+Color.GRAY+" | "+ChatColor.WHITE+"Successfully log out.");
-            Session.remove(Objects.requireNonNull(p.getAddress()).getAddress().toString());
+            session.remove(Objects.requireNonNull(p.getAddress()).getAddress().toString());
         }
 
         if (label.equalsIgnoreCase("test") ) {
